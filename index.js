98acc6db49e4150014878d3547e5e9a6a228fea3
Dexter S <dexter@pop-os.localdomain>
2022-09-10 14:50:47 -0400

    big

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ class Scratch3dexapi {
             return r;
         };
         hi("https://api.chucknorris.io/jokes/random").then((r)=>{ let a=console.log(r.json().then(r=>{return r}));
-    }
+    },
     getArrayIndex (args) {
         let a = args.ARRAY.slice(0, -1).substr(1);
         let b = args.INDEX;
